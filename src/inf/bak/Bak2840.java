4eec23e1b060f7d3e1ade7561fd8135c903b0cda
whwp4151 <whwp4151@naver.com>
2025-07-29 22:50:20 +0900

    feat: 백준 행운의 바퀴

---
+++ src/inf/bak/Bak2840.java
@@ -0,0 +1,50 @@
+package inf.bak;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bak2840 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        char[] wheel = new char[N];
+        Arrays.fill(wheel, '?');
+        int wheelIndex = 0;
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            char c = st.nextToken().charAt(0);
+
+            wheelIndex = ((wheelIndex - S) % N + N) % N;
+            if (wheel[wheelIndex] == '?') {
+                wheel[wheelIndex] = c;
+            } else if (wheel[wheelIndex] != c) {
+                System.out.println("!");
+                return;
+            }
+        }
+
+        boolean[] used = new boolean[26];
+        for (int i = 0; i < N; i++) {
+            if (used[wheel[i] - 'A']) {
+                System.out.println("!");
+                return;
+            }
+            used[wheel[i] - 'A'] = true;
+        }
+
+        for (int i = 0; i < N; i++) {
+            bw.write(wheel[(wheelIndex + i) % N]);
+        }
+
+        bw.flush();
+    }
+
+}
