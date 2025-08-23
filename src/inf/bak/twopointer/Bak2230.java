a2ec347154d7a8ed16b11350491db0052d6c15a7
whwp4151 <whwp4151@naver.com>
2025-08-23 23:15:49 +0900

    fix: 백준 수 고르기 수정

--- src/inf/bak/twopointer/Bak2230.java
+++ src/inf/bak/twopointer/Bak2230.java
@@ -14,26 +14,23 @@ public class Bak2230 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        long[] arr = new long[N];
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(arr);
 
         int j = 0;
-        long answer = 2000000001;
+        int answer = 2000000001;
         for (int i = 0; i < N; i++) {
-            long sub = arr[j] - arr[i];
-            while (sub < M  && j < N - 1) {
-                sub = arr[++j] - arr[i];
+            while (arr[j] - arr[i] < M  && j < N - 1) {
+                j++;
             }
 
-            if (sub > M) {
+            int sub = arr[j] - arr[i];
+            if (sub >= M) {
                 answer = Math.min(answer, sub);
-            } else if (sub == M) {
-                answer = sub;
-                break;
             }
         }
 
