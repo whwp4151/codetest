b5905a6a86d3c4f0f14de5cf19fad1b5051e096a
whwp4151 <whwp4151@naver.com>
2025-09-19 00:02:06 +0900

    fix: 백준 회전초밥 수정

--- src/inf/bak/part1/Bak15961.java
+++ src/inf/bak/part1/Bak15961.java
@@ -21,43 +21,25 @@ public class Bak15961 {
         }
         br.close();
 
-        int answer = 0;
         int cnt = 0;
-        int j = k-1;
         int[] count = new int[d+1];
         for (int i = 0; i < k-1; i++) {
-            if (count[arr[i]] == 0) {
+            if (count[arr[i]]++ == 0) {
                 cnt++;
             }
-            count[arr[i]]++;
         }
-        for (int i = 0; i < N - k + 1; i++) {
-            if (count[arr[j]] == 0) {
+
+        int answer = cnt;
+        for (int i = 0; i < N; i++) {
+            if (count[arr[(i + k - 1) % N]]++ == 0) {
                 cnt++;
             }
 
-            count[arr[j]]++;
+            answer = Math.max(answer, cnt + (count[c] == 0 ? 1 : 0));
 
-            j++;
-
-            boolean isCoupon = false;
-            if (count[c] == 0) {
-                count[c]++;
-                isCoupon = true;
-                cnt++;
-            }
-
-            answer = Math.max(answer, cnt);
-
-            if (isCoupon) {
-                count[c]--;
+            if (count[arr[i]]-- == 1) {
                 cnt--;
             }
-
-            if (count[arr[i]] == 1) {
-                cnt--;
-            }
-            count[arr[i]]--;
         }
 
         bw.write(answer + "\n");
