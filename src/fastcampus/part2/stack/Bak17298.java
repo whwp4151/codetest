c611420cfd8c3c58f6fe2e06cec67d1c98473bbe
whwp4151 <whwp4151@naver.com>
2025-10-15 23:21:59 +0900

    feat: 백준 오큰수

---
+++ src/fastcampus/part2/stack/Bak17298.java
@@ -0,0 +1,43 @@
+package fastcampus.part2.stack;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Bak17298 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        for (int n : arr) {
+            bw.write(n + " ");
+        }
+        bw.newLine();
+
+        bw.flush();
+    }
+
+}
