53b9e3bf34753e003fbb0a700149d590f2ccee66
whwp4151 <whwp4151@naver.com>
2025-10-20 23:18:37 +0900

    feat: 백준 N과 M (7), (8)

---
+++ src/fastcampus/part2/recursion/Bak15657.java
@@ -0,0 +1,54 @@
+package fastcampus.part2.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bak15657 {
+
+    private static int N;
+    private static int M;
+    private static int[] arr;
+    private static int[] output;
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        output = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        Arrays.sort(arr);
+
+        recursion(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void recursion(int depth, int start) {
+        if (depth == M) {
+            for (int i : output) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            output[depth] = arr[i];
+            recursion(depth + 1, i);
+        }
+    }
+
+}
