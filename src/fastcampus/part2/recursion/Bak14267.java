1c3b5dafb76b6f892865d374aa1501601f510461
whwp4151 <whwp4151@naver.com>
2025-10-24 23:42:36 +0900

    feat: 백준 회사 문화 1

--- src/fastcampus/part2/recursion/Bak14267.java
+++ src/fastcampus/part2/recursion/Bak14267.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 public class Bak14267 {
 
-    private static int[] employee;
+    private static List<Integer>[] tree;
+    private static int[] point;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,13 +15,42 @@ public class Bak14267 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        employee = new int[n + 1];
+        tree = new ArrayList[n + 1];
+        point = new int[n + 1];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            employee[i] = Integer.parseInt(st.nextToken());
+            tree[i] = new ArrayList<>();
+            int parent = Integer.parseInt(st.nextToken());
+
+            if (parent != -1) {
+                tree[parent].add(i);
+            }
         }
 
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int index = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            point[index] += w;
+        }
 
+        recursion(1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(point[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void recursion(int node) {
+        List<Integer> list = tree[node];
+        for (int child : list) {
+            point[child] += point[node];
+            recursion(child);
+        }
     }
 
 }
