98d941a2a3343812e996f50987cfa7f293237865
whwp4151 <whwp4151@naver.com>
2025-11-22 00:38:43 +0900

    fix: 백준 계란으로 계란치기

--- src/fastcampus/part2/recursion/Bak16987.java
+++ src/fastcampus/part2/recursion/Bak16987.java
@@ -8,22 +8,79 @@ import java.util.StringTokenizer;
 public class Bak16987 {
 
     private static int N;
-    private static int[][] arr;
+    private static Egg[] arr;
+    private static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][2];
+        arr = new Egg[N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            arr[i] = new Egg(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        System.out.println();
+        recursive(0);
+
+        System.out.println(max);
+    }
+
+    private static void recursive(int index) {
+        if (index == N) {
+            int cnt = 0;
+            for (int i = 0; i < N; i++) {
+                if (arr[i].isBreak()) {
+                    cnt++;
+                }
+            }
+
+            max = Math.max(max, cnt);
+            return;
+        }
+
+        if (!arr[index].isBreak()) {
+            boolean flag = false;
+            for (int i = 0; i < N; i++) {
+                if (i == index) continue;
+                if (!arr[i].isBreak()) {
+                    flag = true;
+                    arr[index].attack(arr[i]);
+                    recursive(index + 1);
+                    arr[index].restore(arr[i]);
+                }
+            }
+            if (!flag) {
+                recursive(index + 1);
+            }
+        } else {
+            recursive(index + 1);
+        }
+    }
+
+    private static class Egg {
+        int weight;
+        int durability;
+
+        public Egg(int durability, int weight) {
+            this.weight = weight;
+            this.durability = durability;
+        }
+
+        public void attack(Egg other) {
+            this.durability -= other.weight;
+            other.durability -= this.weight;
+        }
+
+        public void restore(Egg other) {
+            this.durability += other.weight;
+            other.durability += this.weight;
+        }
+
+        public boolean isBreak() {
+            return this.durability <= 0;
+        }
     }
 
 }
