38ab72f09c9dbc81e60f33597801766eff1813f3
whwp4151 <whwp4151@naver.com>
2025-11-27 22:50:38 +0900

    feat: 백준 색종이 붙이기

---
+++ src/fastcampus/part2/recursion/Bak17136.java
@@ -0,0 +1,25 @@
+package fastcampus.part2.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bak17136 {
+
+    private static int[][] arr = new int[10][10];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 10; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println();
+    }
+
+}
--- src/fastcampus/part2/recursion/Bak1987.java
+++ src/fastcampus/part2/recursion/Bak1987.java
@@ -9,8 +9,12 @@ public class Bak1987 {
 
     private static int R;
     private static int C;
-    private static char[][] arr;
-    private static int max = Integer.MIN_VALUE;
+    private static int[][] arr;
+    private static boolean[] check = new boolean[26];
+    private static int[] dy = {0, 1, 0, -1};
+    public static int[] dx = {1, 0, -1, 0};
+
+    private static int[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,18 +22,52 @@ public class Bak1987 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        arr = new char[R][C];
+        arr = new int[R][C];
+
+        visited = new int[R][C];
 
         for (int i = 0; i < R; i++) {
             String s = br.readLine();
             for (int j = 0; j < C; j++) {
-                arr[i][j] = s.charAt(j);
+                arr[i][j] = s.charAt(j) - 'A';
             }
         }
 
-        
+        visited[0][0] = 1 << arr[0][0];
+        check[arr[0][0]] = true;
+        int max = recursive(0, 0);
 
         System.out.println(max);
+    }
+
+    private static int recursive(int y, int x) {
+        int result = 0;
+        for (int k = 0; k < 4; k++) {
+            int ny = y + dy[k];
+            int nx = x + dx[k];
+
+            if (ny < 0 || ny >= R || nx < 0 || nx >= C) {
+                continue;
+            }
+
+            int num = arr[ny][nx];
+            if (check[num]) {
+                continue;
+            }
+
+            int route = 1 << num;
+            if (visited[ny][nx] == (visited[y][x] | route)) {
+                continue;
+            }
+
+            visited[ny][nx] = visited[y][x] | route;
+
+            check[num] = true;
+            result = Math.max(result, recursive(ny, nx));
+            check[num] = false;
+        }
+
+        return result + 1;
     }
 
 }
