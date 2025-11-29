dad0d90d9831a400cc9b4b5f4da61a2e799e0ddc
whwp4151 <whwp4151@naver.com>
2025-11-29 23:45:41 +0900

    fix: 순열장난

--- src/fastcampus/part2/recursion/Bak10597.java
+++ src/fastcampus/part2/recursion/Bak10597.java
@@ -1,14 +1,27 @@
 package fastcampus.part2.recursion;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Bak10597 {
 
+    private static int N;
+    private static char[] arr;
+    private static boolean[] check = new boolean[101];
+    private static int[] answer;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        arr = br.readLine().toCharArray();
+        N = arr.length <= 9 ? arr.length : (arr.length - 9) / 2 + 9;
+        answer = new int[N];
+
+        for (int n : answer) {
+            bw.write(n + " ");
+        }
+        bw.newLine();
+        bw.flush();
     }
 
 }
