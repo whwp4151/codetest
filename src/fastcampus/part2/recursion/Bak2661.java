d3658b3c8db54e7502da9a5d25f30713ef63a1a4
whwp4151 <whwp4151@naver.com>
2025-12-02 23:35:09 +0900

    feat: 백준 N-Queen

--- src/fastcampus/part2/recursion/Bak2661.java
+++ src/fastcampus/part2/recursion/Bak2661.java
@@ -7,13 +7,53 @@ import java.io.InputStreamReader;
 public class Bak2661 {
 
     private static int N;
+    private static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
+        arr = new int[N];
         br.close();
 
-        
+        recursive(0);
     }
+
+    private static boolean recursive(int index) {
+        if (index == N) {
+            for (int i = 0; i < N; i++) {
+                System.out.print(arr[i]);
+            }
+            return true;
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            arr[index] = i;
+            if (!validate(index)) {
+                if (recursive(index + 1)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean validate(int index) {
+        for (int i = 1; i <= (index + 1) / 2; i++) {
+            boolean flag = true;
+            for (int j = 0; j < i; j++) {
+                if (arr[index - j] != arr[index - i - j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
---
+++ src/fastcampus/part2/recursion/Bak9663.java
@@ -0,0 +1,20 @@
+package fastcampus.part2.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bak9663 {
+
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        br.close();
+
+        
+    }
+
+}
