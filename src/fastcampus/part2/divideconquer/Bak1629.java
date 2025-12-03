94b23dd005a9a3646539104a92450462cb01297c
whwp4151 <whwp4151@naver.com>
2025-12-03 23:44:25 +0900

    feat: 백준 곱셈

---
+++ src/fastcampus/part2/divideconquer/Bak1629.java
@@ -0,0 +1,23 @@
+package fastcampus.part2.divideconquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bak1629 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        
+    }
+
+}
--- src/fastcampus/part2/recursion/Bak9663.java
+++ src/fastcampus/part2/recursion/Bak9663.java
@@ -7,14 +7,48 @@ import java.io.InputStreamReader;
 public class Bak9663 {
 
     private static int N;
+    private static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
+        arr = new int[N];
         br.close();
 
-        
+        int answer = recursive(0);
+        System.out.println(answer);
+    }
+
+    private static int recursive(int row) {
+        if (row == N) {
+            return 1;
+        }
+
+        int count = 0;
+        for (int col = 0; col < N; col++) {
+            if (validate(row, col)) {
+                arr[row] = col;
+                count += recursive(row + 1);
+                System.out.println();
+            }
+        }
+
+        return count;
+    }
+
+    private static boolean validate(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (arr[i] == col) {
+                return false;
+            }
+
+            if (Math.abs(i - row) == Math.abs(arr[i] - col)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
