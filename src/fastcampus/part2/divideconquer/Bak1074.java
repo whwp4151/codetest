233bbcfa8d4dbd45430314a5b697766470b19d86
whwp4151 <whwp4151@naver.com>
2025-12-04 23:32:50 +0900

    feat: 백준 Z

---
+++ src/fastcampus/part2/divideconquer/Bak1074.java
@@ -0,0 +1,21 @@
+package fastcampus.part2.divideconquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bak1074 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        
+    }
+
+}
--- src/fastcampus/part2/divideconquer/Bak1629.java
+++ src/fastcampus/part2/divideconquer/Bak1629.java
@@ -13,11 +13,25 @@ public class Bak1629 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         br.close();
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
 
-        
+        System.out.println(solve(A, B, C));
+    }
+
+    private static long solve(long A, long B, long C) {
+        if (B == 1) {
+            return A % C;
+        }
+
+        long half = solve(A, B / 2, C);
+
+        if (B % 2 == 0) {
+            return (half * half) % C;
+        } else {
+            return (((half * half) % C) * A) % C;
+        }
     }
 
 }
