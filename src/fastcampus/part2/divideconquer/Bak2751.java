ef2a63580cc92dcff2251ba12e05a383746b8701
whwp4151 <whwp4151@naver.com>
2025-12-09 23:35:36 +0900

    feat: 백준 좋은 친구

--- src/fastcampus/part2/divideconquer/Bak2751.java
+++ src/fastcampus/part2/divideconquer/Bak2751.java
@@ -1,30 +1,70 @@
 package fastcampus.part2.divideconquer;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class Bak2751 {
 
     private static int[] arr;
+    private static int[] temp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         arr = new int[N];
+        temp = new int[N];
 
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         br.close();
 
-        Arrays.sort(arr);
+        divide(0, N - 1);
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            bw.write(arr[i] + "\n");
+           sb.append(temp[i]).append("\n");
         }
-        bw.flush();
+        System.out.println(sb);
+    }
+
+    private static void divide(int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        divide(start, mid);
+        divide(mid + 1, end);
+
+        conquer(start, end);
+    }
+
+    private static void conquer(int start, int end) {
+        int mid = (start + end) / 2;
+        int l = start;
+        int r = mid + 1;
+        int index = start;
+
+        while (l <= mid && r <= end) {
+            if (arr[l] < arr[r]) {
+                temp[index++] = arr[l++];
+            } else {
+                temp[index++] = arr[r++];
+            }
+        }
+
+        while (l <= mid) {
+            temp[index++] = arr[l++];
+        }
+
+        while (r <= end) {
+            temp[index++] = arr[r++];
+        }
+
+        for (int i = start; i <= end; i++) {
+            arr[i] = temp[i];
+        }
     }
 
 }
---
+++ src/fastcampus/part2/quiz/Bak3078.java
@@ -0,0 +1,24 @@
+package fastcampus.part2.quiz;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Bak3078 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue<String> queue = new LinkedList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        
+    }
+
+}
