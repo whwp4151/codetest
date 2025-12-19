882f0e4bd64a0c2d6b5460fcc7837ed5f5a8d29c
whwp4151 <whwp4151@naver.com>
2025-12-20 00:55:53 +0900

    fix: 백준 DFS와 BFS

--- src/fastcampus/part3/graph/Bak1260.java
+++ src/fastcampus/part3/graph/Bak1260.java
@@ -3,41 +3,32 @@ package fastcampus.part3.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Bak1260 {
 
-    private static List<Integer>[] tree;
+    private static int N;
+    private static int[][] tree;
     private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
-        tree = new ArrayList[N + 1];
+        tree = new int[N + 1][N + 1];
         visited = new boolean[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            tree[i] = new ArrayList<>();
-        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
-            tree[n1].add(n2);
+            tree[n1][n2] = 1;
+            tree[n2][n1] = 1;
         }
         br.close();
-
-        for (int i = 1; i <= N; i++) {
-            Collections.sort(tree[i]);
-        }
 
         dfs(V);
 
@@ -47,24 +38,33 @@ public class Bak1260 {
         System.out.println();
 
         bfs(V);
+        System.out.println();
     }
 
     private static void dfs(int node) {
         System.out.print(node + " ");
         visited[node] = true;
-        for (int child : tree[node]) {
-            if (!visited[child]) {
-                dfs(child);
+        for (int i = 1; i <= N; i++) {
+            if (tree[node][i] == 1 && !visited[i]) {
+                dfs(i);
             }
         }
     }
 
     private static void bfs(int node) {
-        for (int child : tree[node]) {
-            if (!visited[child]) {
-                System.out.print(child + " ");
-                visited[child] = true;
-                bfs(child);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+            System.out.print(poll + " ");
+
+            for (int i = 1; i <= N; i++) {
+                if (tree[poll][i] == 1 && !visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+                }
             }
         }
     }
