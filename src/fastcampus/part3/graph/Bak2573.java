ec927059c9645e0ede8529a6ed887a44f2d0dabf
whwp4151 <whwp4151@naver.com>
2025-12-22 23:36:54 +0900

    feat: 백준 빙산

---
+++ src/fastcampus/part3/graph/Bak2573.java
@@ -0,0 +1,32 @@
+package fastcampus.part3.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bak2573 {
+
+    private static int N, M;
+    private static int[][] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        br.close();
+
+        System.out.println();
+    }
+
+}
