77852806fd576d4841adad0d39c20d4ef4ce133c
whwp4151 <whwp4151@naver.com>
2026-02-10 23:37:22 +0900

    feat: 백준 숨바꼭질

---
+++ src/fastcampus/part3/bfs/Bak1697.java
@@ -0,0 +1,58 @@
+package fastcampus.part3.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Bak1697 {
+
+    private static int N;
+    private static int K;
+
+    private static int[] visited = new int[100_001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        br.close();
+
+        bfs();
+    }
+
+    private static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+        visited[N] = 1;
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+
+            if (poll == K) {
+                System.out.println(visited[K]-1);
+                return;
+            }
+
+            int[] next = {poll - 1, poll + 1, poll * 2};
+            for (int i = 0; i < 3; i++) {
+                if (!isRange(next[i])) {
+                    continue;
+                }
+                if (visited[next[i]] == 0) {
+                    queue.add(next[i]);
+                    visited[next[i]] = visited[poll] + 1;
+                }
+            }
+        }
+    }
+
+    private static boolean isRange(int x) {
+        return x >= 0 && x < 100_001;
+    }
+
+}
--- src/fastcampus/part3/bfs/Bak2178.java
+++ src/fastcampus/part3/bfs/Bak2178.java
@@ -3,6 +3,8 @@ package fastcampus.part3.bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Bak2178 {
@@ -10,7 +12,10 @@ public class Bak2178 {
     private static int N;
     private static int M;
     private static int[][] arr;
-    private static boolean[][] visited;
+    private static int[][] visited;
+
+    private static final int[] dx = {0, 0, -1, 1};
+    private static final int[] dy = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,17 +25,57 @@ public class Bak2178 {
         M = Integer.parseInt(st.nextToken());
 
         arr = new int[N][M];
-        visited = new boolean[N][M];
+        visited = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+        br.close();
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0, 0));
+        visited[0][0] = 1;
+
+        while (!queue.isEmpty()) {
+            boolean flag = false;
+            Node poll = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int r = poll.r + dy[i];
+                int c = poll.c + dx[i];
+
+                if (r < 0 || r >= N || c < 0 || c >= M) {
+                    continue;
+                }
+
+                if (visited[r][c] == 0 && arr[r][c] == 1) {
+                    queue.add(new Node(r, c));
+                    visited[r][c] = visited[poll.r][poll.c] + 1;
+                }
+
+                if (r == N-1 && c == M-1) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag) {
+                break;
             }
         }
 
+        System.out.println(visited[N-1][M-1]);
     }
 
-
+    private static class Node {
+        int r, c;
+        public Node(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
 
 }
