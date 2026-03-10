2a4a485a24b81c2b382bfa29fb9030eb0740fd3a
whwp4151 <whwp4151@naver.com>
2026-03-10 23:39:09 +0900

    feat: 피보나치 수 2

--- src/fastcampus/part3/dfs/Bak9466.java
+++ src/fastcampus/part3/dfs/Bak9466.java
@@ -14,33 +14,43 @@ public class Bak9466 {
         while (T-- > 0) {
             int n = Integer.parseInt(br.readLine());
             int[] arr = new int[n + 1];
-            boolean[] visited = new boolean[n + 1];
+            int[] depth = new int[n + 1];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= n; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            solution(n, arr, visited);
+            solution(n, arr, depth);
         }
 
         bw.flush();
     }
 
-    private static void solution(int n, int[] arr, boolean[] visited) {
-
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                dfs(i, arr, visited);
+    private static void solution(int N, int[] arr, int[] depth) throws IOException {
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (depth[i] == 0) {
+                depth[i] = 1;
+                count += dfs(i, arr, depth);
             }
         }
+
+        bw.write((N - count) + "\n");
     }
 
-    private static void dfs(int index, int[] arr, boolean[] visited) {
-        visited[index] = true;
-        if (!visited[arr[index]]) {
-            dfs(arr[index], arr, visited);
+    private static int dfs(int index, int[] arr, int[] depth) {
+        int next = arr[index];
+        int cycleCnt;
+        if (depth[next] == 0) {
+            depth[next] = depth[index] + 1;
+            cycleCnt = dfs(next, arr, depth);
+        } else {
+            cycleCnt = depth[index] - depth[next] + 1;
         }
+
+        depth[index] = 100001;
+        return Math.max(cycleCnt, 0);
     }
 
 }
---
+++ src/fastcampus/part3/dp/Bak2748.java
@@ -0,0 +1,18 @@
+package fastcampus.part3.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bak2748 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        
+    }
+
+}
