4cd5ee44e3887280f4c59d0962fcd5d33cbcf292
whwp4151 <whwp4151@naver.com>
2026-03-11 23:16:34 +0900

    feat: 백준 2×n 타일링 2

---
+++ src/fastcampus/part3/dp/Bak11727.java
@@ -0,0 +1,26 @@
+package fastcampus.part3.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bak11727 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
--- src/fastcampus/part3/dp/Bak2748.java
+++ src/fastcampus/part3/dp/Bak2748.java
@@ -6,13 +6,52 @@ import java.io.InputStreamReader;
 
 public class Bak2748 {
 
+    private static final long[] memory = new long[91];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
         br.close();
 
-        
+        fibo3(n);
+    }
+
+    private static long fibo1(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        if (memory[n] != 0) {
+            return memory[n];
+        }
+
+        memory[n] = fibo1(n - 1) + fibo1(n - 2);
+
+        return memory[n];
+    }
+
+    private static void fibo2(int n) {
+        long[] arr = new long[91];
+        arr[1] = 1;
+        arr[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        System.out.println(arr[n]);
+    }
+
+    private static void fibo3(int n) {
+        long[] arr = new long[92];
+        arr[1] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            arr[i+1] = arr[i] + arr[i-1];
+        }
+
+        System.out.println(arr[n]);
     }
 
 }
