1e24b18f4b58d69d36f5c45aa2c1270cf4e31296
whwp4151 <whwp4151@naver.com>
2026-03-26 23:35:49 +0900

    feat: 백준 동전 0

---
+++ src/fastcampus/part3/dp/Bak11047.java
@@ -0,0 +1,24 @@
+package fastcampus.part3.dp;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Bak11047 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+
+        System.out.println();
+    }
+
+}
