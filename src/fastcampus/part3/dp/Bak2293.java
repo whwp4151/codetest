8cfc05da24ae22b7a2b24b95560221a055282ea9
whwp4151 <whwp4151@naver.com>
2026-03-29 01:28:24 +0900

    feat: 백준 동전 1

---
+++ src/fastcampus/part3/dp/Bak2293.java
@@ -0,0 +1,26 @@
+package fastcampus.part3.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bak2293 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] dp = new int[K + 1];
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+
+
+    }
+
+}
--- src/fastcampus/part3/dp/Bak2294.java
+++ src/fastcampus/part3/dp/Bak2294.java
@@ -13,14 +13,36 @@ public class Bak2294 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        int[] dp = new int[K + 1];
+        for (int i = 1; i <= K; i++) {
+            dp[i] = 100_001;
+        }
 
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
+            if (arr[i] <= K) {
+                dp[arr[i]] = 1;
+            }
         }
         br.close();
 
-        
+        for (int i = 1; i <= K; i++) {
+            for (int j = 0; j < N; j++) {
+//                if (i + arr[j] <= K) {
+//                    dp[i + arr[j]] = Math.min(dp[i + arr[j]], dp[i] + 1);
+//                }
+                if (i - arr[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - arr[j]] + 1);
+                }
+            }
+        }
+
+        if (dp[K] == 100_001) {
+            System.out.println("-1");
+        } else {
+            System.out.println(dp[K]);
+        }
     }
 
 }
