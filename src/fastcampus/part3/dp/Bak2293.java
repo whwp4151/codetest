57b8c0ac8c16195af1f5a8a1ce8e9c05a15921af
whwp4151 <whwp4151@naver.com>
2026-03-29 23:11:18 +0900

    feat: 백준 동전 바꿔주기

--- src/fastcampus/part3/dp/Bak2293.java
+++ src/fastcampus/part3/dp/Bak2293.java
@@ -14,13 +14,20 @@ public class Bak2293 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int[] dp = new int[K + 1];
-        int[] arr = new int[N];
+        int[] coin = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            coin[i] = Integer.parseInt(br.readLine());
         }
         br.close();
 
+        dp[0] = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = coin[i]; j <= K; j++) {
+                dp[j] += dp[j - coin[i]];
+            }
+        }
 
+        System.out.println(dp[K]);
     }
 
 }
---
+++ src/fastcampus/part3/dp/Bak2624.java
@@ -0,0 +1,26 @@
+package fastcampus.part3.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bak2624 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+        int[][] coin = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            coin[i][0] = Integer.parseInt(st.nextToken());
+            coin[i][1] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        System.out.println();
+    }
+
+}
