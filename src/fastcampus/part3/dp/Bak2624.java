6e2eb9309cc06c18437b1d22c54a89bd502152d6
whwp4151 <whwp4151@naver.com>
2026-03-31 23:55:54 +0900

    fix: 동전 바꿔주기

--- src/fastcampus/part3/dp/Bak2624.java
+++ src/fastcampus/part3/dp/Bak2624.java
@@ -13,14 +13,14 @@ public class Bak2624 {
         int T = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
         int[][] coin = new int[k][2];
+        int[][] dp = new int[k + 1][T + 1];
         for (int i = 0; i < k; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             coin[i][0] = Integer.parseInt(st.nextToken());
             coin[i][1] = Integer.parseInt(st.nextToken());
         }
-        br.close();
 
-        System.out.println();
+        System.out.println(dp[k][T]);
     }
 
 }
